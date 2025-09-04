27eeb0313275ef287a42b08428cfc0877c98cc30
jaovitim <162746128+jaovitim590@users.noreply.github.com>
2025-09-04 18:07:05 -0300

    Update fetchApis.js

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,9 +1,9 @@
-const API_URL = "https://projeto-alura.onrender.com/posts"; // URL direta da API
+import { CONFIG } from "./config.js";
 
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(API_URL); // usando a URL direta
+    const response = await fetch(`${CONFIG.API_URL}/posts`); // usa a URL do config
     const data = await response.json();
     return data;
   } catch (error) {
